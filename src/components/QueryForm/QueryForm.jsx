f9c4690346b527160b8b579ed57e404acd7adbe9
vokur13 <vokur13@gmail.com>
2022-10-04 18:55:49 +0300

    project init

---
+++ src/components/QueryForm/QueryForm.jsx
@@ -0,0 +1,3 @@
+import { Formik } from 'formik';
+
+export const QueryForm = () => {};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'modern-normalize/modern-normalize.css';
-// import 'react-toastify/dist/ReactToastify.css';
 import { App } from './App/App';
-import './index.css';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
